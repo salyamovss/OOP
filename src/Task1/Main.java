7a1a676ec4a7bc29dd6a5954d9225c24710a6cca
m1 <symbat.saliamov@alatoo.edu.kg>
2025-03-17 20:07:30 +0600

    testing git remote

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -7,5 +7,7 @@ public class Main {
         Passport passport = new Passport(1,"Symbat", "Saliamov",LocalDate.ofYearDay(2006,7),'M',"Kyrgyzstan");
         System.out.println(passport.toString());
 
+      // todo: Test project
+
     }
 }
